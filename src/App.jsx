90abd18abb85490fe52c29040c240c118ff30d32
Victor Inojosa <vijoin@users.noreply.github.com>
2022-02-28 23:40:41 +0000

    Connect wallet using 3rdWeb

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+// import { useEffect, useMemo, useState } from "react";
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("Hello Address:", address)
+
+  if (!address) {
+    return (
+      <div>
+        <h1>Welcome to 0x58DA0</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet connected! Now what?</h1>
     </div>
   );
+  
 };
 
 export default App;
