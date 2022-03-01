b572a173be5997f2bfff457f1579debaf2475738
Victor Inojosa <vijoin@users.noreply.github.com>
2022-03-01 13:20:36 +0000

    Create script to get ThirdWeb app address

---
+++ scripts/1-initialize-sdk.js
@@ -0,0 +1,37 @@
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import ethers from "ethers";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY == "") {
+  console.log("ERROR: Private key not foud.")
+}
+
+if (!process.env.ALCHEMY_API_URL || process.env.ALCHEMY_API_URL == "") {
+  console.log("ERROR: Alchemy API URL not found.")
+}
+
+if (!process.env.WALLET_ADDRESS || process.env.WALLET_ADDRESS == "") {
+  console.log("ERROR: WALLET ADDRESS not found.")
+}
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.PRIVATE_KEY,
+    ethers.getDefaultProvider(process.env.ALCHEMY_API_URL),
+  ),
+);
+
+(async () => {
+  try {
+    const apps = await sdk.getApps();
+    console.log("Your app address is:", apps[0].address);
+  } catch (error) {
+    console.error("Failed to get apps from the sdk")
+    process.exit(1);
+  }
+})();
+
+
+export default sdk;
